c1cbc2312819ef53b2fe8f1fb73570513c00e86c
Palak Mody <prmody@yahoo.com>
2011-06-28 14:01:26 -0700

    initial commit on all files

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ gem 'rails', '3.0.9'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'23
+gem 'sqlite3'
 
 # Use unicorn as the web server
 # gem 'unicorn'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'bd4db6f5e4cdda4a4c5157ce94c1e691f0d1a030d83b3e703d32eae0468f62f07389d8ade0dcb35330e375e16c2c8b0edd9f47d1052553a2a0900f0c8f4bc5a9'
